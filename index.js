fbe14e8bdb4f6ddb4990d7c094d682f8f64aef31
Misbahsajib <matealo1186.eg@gmail.com>
2022-09-22 13:03:01 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express()
+const cors = require('cors')
+const port = process.env.PORT || 5000
+require('dotenv').config()
+app.use(cors())
+app.use(express.json())
+// 
+
+
+const uri = `mongodb+srv://${process.env.useer_name}:${process.env.user_password}@cluster0.4tiocny.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const serverCollection = client.db("newcar").collection("carservice");
+        const OrderCollection = client.db("newcar").collection("order");
+        app.get('/server', async (req, res) => {
+            const query = {};
+            const cursor = serverCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services)
+        });
+        // find data
+        app.get('/server/:id', async (req, res) => {
+            const id=req.params.id;
+           const query={_id:ObjectId(id)};
+           const server=await serverCollection.findOne(query)
+           res.send(server)
+        })
+        // post
+        app.post('/server',async(req,res)=>{
+            const newservice=req.body;
+            const result=await serverCollection.insertOne(newservice)
+            res.send(result)
+        })
+        // delete
+        app.delete('server/:id',async(req,res)=>{
+            const id=req.params.id;
+            const query={_id:ObjectId(id)}
+            const delet=await serverCollection.deleteOne(query)
+            res.send(delet)
+        })
+
+        
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
