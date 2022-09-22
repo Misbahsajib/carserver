3c2375c7419f8f0698def4b883407d1f34be9074
Misbahsajib <matealo1186.eg@gmail.com>
2022-09-22 21:01:23 +0600

    adding new

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const cors = require('cors')
-const port = process.env.PORT || 5000
 require('dotenv').config()
+const port = process.env.PORT || 5000
+
 app.use(cors())
 app.use(express.json())
 // 
