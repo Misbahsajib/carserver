b8bf050d28ed82fa6daa551f8263817d73bcfd7e
Misbahsajib <matealo1186.eg@gmail.com>
2022-09-26 11:35:58 +0600

    new heroku

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('Hello world you are connected!')
+    res.send(' you are connected!')
 })
 
 app.listen(port, () => {
